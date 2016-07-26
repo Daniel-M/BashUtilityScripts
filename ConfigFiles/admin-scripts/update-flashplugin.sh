fcab9bdbbe1737a671d79de0784f5df336124a7d
Daniel-M <danielmejia55@gmail.com>
2016-07-25 19:43:43 -0500

    Minor changes to script for update flash-plugin

--- ConfigFiles/admin-scripts/update-flashplugin.sh
+++ ConfigFiles/admin-scripts/update-flashplugin.sh
@@ -2,8 +2,9 @@ actual_version=$(update-flashplugin-nonfree --status | head -n 1 | tail -n 1 | a
 upstream_version=$(update-flashplugin-nonfree --status | head -n 2 | tail -n 1 | awk -F": " '{print $2}')
 if [ "$actual_version" = "$upstream_version" ]; then
 #    echo "Doesn't require update"
+    export DISPLAY=:0.0 && sudo -u damejia /usr/bin/notify-send -i "/usr/share/icons/Tango/32x32/emblems/emblem-important.png" "Notice:" "flash plugin in the latest version $actual_version."
 else
 #    echo "Requires update"
     update-flashplugin-nonfree --install
-    export DISPLAY=:0.0 && sudo -u damejia /usr/bin/notify-send -i "/usr/share/icons/Tango/32x32/emblems/emblem-important.png" "Notice:" "flash plugin updated."
+    export DISPLAY=:0.0 && sudo -u damejia /usr/bin/notify-send -i "/usr/share/icons/Tango/32x32/emblems/emblem-important.png" "Notice:" "flash plugin updated to version $upstream_version."
 fi
