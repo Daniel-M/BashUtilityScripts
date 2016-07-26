71258544423c42f40711aeccf105dc22d7a6b3f1
Daniel-M <danielmejia55@gmail.com>
2016-07-25 19:36:58 -0500

    Adding autoupdate for flash-plugin

---
+++ ConfigFiles/admin-scripts/update-flashplugin.sh
@@ -0,0 +1,9 @@
+actual_version=$(update-flashplugin-nonfree --status | head -n 1 | tail -n 1 | awk -F": " '{print $2}')
+upstream_version=$(update-flashplugin-nonfree --status | head -n 2 | tail -n 1 | awk -F": " '{print $2}')
+if [ "$actual_version" = "$upstream_version" ]; then
+#    echo "Doesn't require update"
+else
+#    echo "Requires update"
+    update-flashplugin-nonfree --install
+    export DISPLAY=:0.0 && sudo -u damejia /usr/bin/notify-send -i "/usr/share/icons/Tango/32x32/emblems/emblem-important.png" "Notice:" "flash plugin updated."
+fi
